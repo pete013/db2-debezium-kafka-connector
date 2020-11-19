137f0a8410c08bdf9a90e7b46947040c9b5a1869
Pritish <pritish.jain@telenetgroup.be>
2020-11-19 23:37:49 +0100

    initialize db2 only once

--- data/db2/scripts/user/setup_db.sh
+++ data/db2/scripts/user/setup_db.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+if [ ! -f /db2/scripts/setup.nlk ]; then
 su - db2inst1 -c "
   db2 connect to $DBNAME;
   db2 -tvmf /db2/scripts/01_create_tables.sql;
@@ -7,3 +8,7 @@ su - db2inst1 -c "
   db2 -tvmf /db2/scripts/03_populate_tables.sql;
   db2 connect reset;
 "
+fi
+touch /db2/scripts/setup.nlk
+
+echo "setup done"
