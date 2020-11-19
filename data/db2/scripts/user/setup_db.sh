4033f8368efce7eaccc9d1cf012bcf8a2adcf92e
Pritish <pritish.jain@telenetgroup.be>
2020-11-19 23:04:35 +0100

    db2 cdc working

--- data/db2/scripts/user/setup_db.sh
+++ data/db2/scripts/user/setup_db.sh
@@ -3,7 +3,7 @@
 su - db2inst1 -c "
   db2 connect to $DBNAME;
   db2 -tvmf /db2/scripts/01_create_table_orders.sql;
-  db2 -tvmf /db2/scripts/02_enable_cdc_orders.sql;
+  db2 -tvmf /db2/scripts/02_enable_cdc.sql;
   db2 -tvmf /db2/scripts/03_populate_orders.sql;
   db2 connect reset;
 "
